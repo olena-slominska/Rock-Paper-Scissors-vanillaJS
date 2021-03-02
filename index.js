7baf132546eb55d8c2c5bb4b71e22804dc6a3f1f
Olena <64963088+olena-slominska@users.noreply.github.com>
2021-03-02 14:44:14 +0200

    Initial upload from local repo

---
+++ index.js
@@ -0,0 +1,124 @@
+const choices = document.querySelectorAll(".choice-item");
+const scoreDiv = document.querySelector(".header-scoreBox_score");
+const resultDiv = document.querySelector(".gameResult");
+const result = document.querySelector(".gameResult-text");
+const restart = document.querySelector(".gameResult-button");
+const playDiv = document.querySelector(".play");
+const houseChoiceImg = document.querySelector(".play-houseChoice_img");
+const myChoiceImg = document.querySelector(".play-myChoice_img");
+const choiceDiv = document.querySelector(".choice");
+const VARIANTS = [
+  {
+    name: "rock",
+    beats: "scissors",
+  },
+  {
+    name: "paper",
+    beats: "rock",
+  },
+  {
+    name: "scissors",
+    beats: "paper",
+  },
+  {
+    name: "lizard",
+    beats: "spock",
+  },
+  {
+    name: "spock",
+    beats: "scissors",
+  },
+  {
+    name: "scissors",
+    beats: "lizard",
+  },
+  {
+    name: "paper",
+    beats: "spock",
+  },
+  {
+    name: "rock",
+    beats: "lizard",
+  },
+  {
+    name: "lizard",
+    beats: "paper",
+  },
+  {
+    name: "spock",
+    beats: "rock",
+  },
+];
+let score = 0;
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", play);
+});
+
+function play(e) {
+  choiceDiv.style.display = "none";
+  restart.style.display = "block";
+  playDiv.style.display = "flex";
+  resultDiv.style.display = "flex";
+  const myChoice = e.target.id;
+  myChoiceImg.style.backgroundColor = "#fff";
+  myChoiceImg.classList.add(myChoice);
+  const variants = VARIANTS.filter((v) => v.name === myChoice);
+  const houseChoice = getHouseChoice();
+  const winner = getWinner(myChoice, houseChoice, variants);
+
+  setTimeout(() => {
+    showHouseChoice(houseChoice);
+    setTimeout(() => {
+      showWinner(winner, score);
+    }, 1200);
+  }, 800);
+}
+function getHouseChoice() {
+  const randomIndex = Math.floor(Math.random() * VARIANTS.length);
+  return VARIANTS[randomIndex].name;
+}
+function showHouseChoice(houseChoice) {
+  houseChoiceImg.classList.add(houseChoice);
+  houseChoiceImg.style.backgroundColor = "#fff";
+}
+function getWinner(myChoice, houseChoice, variants) {
+  if (myChoice === houseChoice) return "draw";
+  if (variants.find((v) => v.beats === houseChoice)) return "meWinner";
+  else return "houseWinner";
+}
+
+function updateScore(value) {
+  score += value;
+  scoreDiv.innerHTML = score;
+}
+
+function showWinner(winner, score) {
+  if (winner === "meWinner") {
+    result.innerHTML = "You win :)";
+    updateScore(1);
+  }
+  if (winner === "houseWinner") {
+    result.innerHTML = "You lose :(";
+    if (+score !== 0) {
+      updateScore(-1);
+    }
+  }
+  if (winner === "draw") {
+    result.innerHTML = "It's a draw!";
+  }
+}
+restart.addEventListener("click", restartGame);
+function restartGame() {
+  myChoiceImg.classList.remove(
+    myChoiceImg.classList[myChoiceImg.classList.length - 1]
+  );
+  houseChoiceImg.classList.remove(
+    houseChoiceImg.classList[houseChoiceImg.classList.length - 1]
+  );
+  result.innerHTML = "";
+  choiceDiv.style.display = "flex";
+  restart.style.display = "none";
+  playDiv.style.display = "none";
+  resultDiv.style.display = "none";
+}
